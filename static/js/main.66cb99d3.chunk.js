a0eb37746e86cbe2d5a0ad697b5f84f58fbea057
MatYou <matyou.kwiatkowsky@gmail.com>
2021-12-02 19:09:05 +0100

    Updates

--- static/js/main.66cb99d3.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonprandom-quotes"]=this["webpackJsonprandom-quotes"]||[]).push([[0],{11:function(t,n,e){"use strict";e.r(n);var c=e(1),s=e.n(c),o=e(3),i=e.n(o),r=(e(8),e(9),e(0)),a=function(){return Object(r.jsxs)("main",{className:"App",children:[Object(r.jsx)("h1",{children:"quotations"}),Object(r.jsx)("section",{})]})};i.a.render(Object(r.jsx)(s.a.StrictMode,{children:Object(r.jsx)(a,{})}),document.getElementById("root"))},8:function(t,n,e){},9:function(t,n,e){}},[[11,1,2]]]);
-//# sourceMappingURL=main.66cb99d3.chunk.js.map
