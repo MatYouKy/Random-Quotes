f9ee619b35e517a043a1d6fba88e90ba3455e8f2
MatYou <matyou.kwiatkowsky@gmail.com>
2021-12-02 17:16:39 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react/cjs/react.development';
+import { useRef } from 'react';
 import './App.css';
 
 function App() {
+  const [quotes, setQuotes] = useState();
+  const [quote, setQuote] = useState({
+    quote:'',
+    author: ''
+  })
+  
+  const random = () => Math.floor((Math.random() * quotes.length));
+  
+  const handleNextQuote = () => {
+    const citat = quotes[random()]
+    setQuote(citat);
+  }
+  
+  const prevQuoteRef = useRef();
+  const prevQuote = prevQuoteRef.current
+
+  const handlePreviousQuote = () => setQuote(prevQuote);
+  
+  useEffect(() => {
+    prevQuoteRef.current = quote;
+  })
+  
+  useEffect(() => {
+    const getQuote = () => {
+      if(quotes) {
+        const citat = quotes[random()]
+        setQuote(citat);
+      }
+    }
+    getQuote();
+  },[quotes])
+
+  
+  const url = 'https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json'
+  useEffect(() => {
+      fetch(url)
+      .then(res => res.json())
+      .then(data => setQuotes(data))
+      .catch(err => {
+          if(err) alert(err)
+      })
+  },[url])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <h1>quotations</h1>
+      <section>
+        <h2 className="quote">Quote: "{quote.quote}"</h2>
+        <h3 className="author">Author: {quote.author}</h3>
+        {prevQuote && prevQuote.author !== '' && <button className="button" onClick={handlePreviousQuote}>Previous</button>}
+        <button className="button" onClick={handleNextQuote}>Next</button>
+      </section>
+    </main>
   );
 }
 
